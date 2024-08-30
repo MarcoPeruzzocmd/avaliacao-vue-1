0366e47985611762ff08ddd87a9001cdeea8d060
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2024-08-30 16:52:54 -0300

    FEITO

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MainComp from './components/MainComp.vue';
+
+</script>
+
+<template>
+  <main-comp />
+</template>
+
+<style scoped></style>
---
+++ src/components/AdicionarTarefa.vue
@@ -0,0 +1,34 @@
+<script setup>
+const model = defineModel()
+
+const props = defineProps(['textoPreenchido']);
+const emit = defineEmits(['update:textoPreenchido', 'adicionarTarefa']);
+
+
+function adicionar() {
+    if (props.textoPreenchido!== '') {
+        emit('adicionarTarefa');
+    }
+}
+</script>
+
+<template>
+    <section class="card">
+        <h2>Adicione uma nova tarefa</h2>
+        <input type="text" v-model="model" @keyup.enter="$emit('adicionarTarefa')" />
+        <button @click="adicionar">Adicionar</button>
+    </section>
+</template>
+
+<style scoped>
+input {
+    width: 50%;
+    padding: 5px;
+    margin: 5px 0;
+}
+
+button {
+    padding: 5px;
+    margin: 5px 0;
+}
+</style>
